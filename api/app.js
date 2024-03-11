6e9d6d7747ab55063d9311e93260ee00e934e6ce
timliftiev <tliftiev2@gmail.com>
2024-03-11 13:51:40 +0200

    lab-3 done

--- api/app.js
+++ api/app.js
@@ -1,16 +1,66 @@
-const app = require("./server");
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const jsonParser = express.json();
+const {
+  connectMongoDB,
+  closeMongoDB,
+  getUsers,
+  getUser,
+  insertUser,
+  removeUserByName,
+  updateUser,
+  removeAllUsers,
+} = require("./mongo");
 
 const host = process.env.API_URL;
 const port = process.env.API_PORT;
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello world</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/api/users", async (req, res) => {
+  await connectMongoDB();
+  const users = await getUsers();
+  await closeMongoDB();
+  res.json(users);
 });
-app.get("/about", (request, response) => {
-  response.send("<h1>О сайте</h1>");
+
+app.get("/api/users/:id", async (req, res) => {
+  await connectMongoDB();
+  const user = await getUser(req.params.id);
+  await closeMongoDB();
+  res.json(user);
 });
-app.get("/contact", (request, response) => {
-  response.send("<h1>Контакты</h1>");
+
+app.post("/api/users", jsonParser, async (req, res) => {
+  await connectMongoDB();
+  await insertUser(req.body);
+  await closeMongoDB();
+  res.json(req.body);
+});
+
+app.delete("/api/users/", async (req, res) => {
+  await connectMongoDB();
+  await removeAllUsers();
+  await closeMongoDB();
+  res.json({ message: "All users removed" });
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+  await connectMongoDB();
+  await removeUserByName(req.params.id);
+  await closeMongoDB();
+  res.json({ message: "User removed", _id: req.params.id });
+});
+
+app.put("/api/users/", jsonParser, async (req, res) => {
+  await connectMongoDB();
+  const response = await updateUser(req.body);
+  await closeMongoDB();
+  res.json(response);
 });
 
 app.listen(port, () => {
